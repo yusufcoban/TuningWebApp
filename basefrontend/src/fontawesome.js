385d9fc4a5e2ace9d6d9138ded7510b85cf3494f
Yusuf Coban <coban@schnaithmann.de>
2024-12-09 14:44:48 +0100

    start cleaning

--- basefrontend/src/fontawesome.js
+++ basefrontend/src/fontawesome.js
@@ -1,9 +1,9 @@
 // src/fontawesome.js
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faUser, faLock, faSignInAlt,faPlus,faTrashCan } from '@fortawesome/free-solid-svg-icons'
+import { faUser, faLock, faHouse, faSignInAlt, faPlus, faTrashCan, faRightFromBracket} from '@fortawesome/free-solid-svg-icons'
 
 // Add icons to the library
-library.add(faUser, faLock, faSignInAlt, faPlus,faTrashCan)
+library.add(faUser, faLock, faSignInAlt, faHouse, faPlus, faTrashCan, faRightFromBracket)
 
 export default FontAwesomeIcon
